f8857dd970d5bbfa0d8a0bbf4e5af567d437011b
FatlemYo <122469156+Fatlem@users.noreply.github.com>
2025-11-27 18:53:20 +0700

    Update InfoCards.jsx
    Update InfoCards.jsx

--- src/components/InfoCards.jsx
+++ src/components/InfoCards.jsx
@@ -3,7 +3,7 @@ const InfoCards = () => {
     {
       id: 1,
       title: 'Fasilitas',
-      description: 'Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.',
+      description: 'Ruang Kelas, Perpustakaan, Toilet, kantin Dll.',
       icon: (
         <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"/>
@@ -13,7 +13,7 @@ const InfoCards = () => {
     {
       id: 2,
       title: 'Lokasi',
-      description: 'Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.',
+      description: 'Jl. Cumi-cumi Raya No 2, Kec. Semarang Utara, Kota Semarang, Prov. Jawa Tengah.',
       icon: (
         <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
@@ -30,10 +30,16 @@ const InfoCards = () => {
         </svg>
       ),
     },
-    {
+      {
       id: 4,
       title: 'Prestasi',
-      description: 'Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.',
+      description: (
+        <ul className="list-disc list-inside text-gray-700 space-y-2 text-sm md:text-base">
+          <li>Juara 3 Lomba Vokal Group</li>
+          <li>Juara 3 Lomba FBBI Cabang Macc. Goggilang Putri</li>
+          <li>Juara 3 Lomba FBBI Cabang Macc. Kalegapi Putri</li>
+        </ul>
+      ),
       icon: (
         <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
